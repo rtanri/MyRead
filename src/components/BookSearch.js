ae00235b5e376a7b189da8050c88412bdbe4e453
Revin Tanri <tanrirevin@gmail.com>
2020-04-18 19:17:24 +0800

    Draft the BookSearch.js

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -6,7 +6,7 @@ import Book from "./Book";
 class BookSearch extends Component {
     state= {
         request: '',
-        searchResult: []
+        searchResults: []
     }
 
     updateSearch = (request) => {
@@ -18,15 +18,64 @@ class BookSearch extends Component {
         // If user type word in search bar, this func look for book that match
         if (search) {
             BooksAPI.search(request).then ((searchResult) => {
-                if (searchResult.error) {
-                    this.setState({ searchResult: [] })
+                if (searchResults.error) {
+                    this.setState({ searchResults: [] })
                 } else {
-                    this.setState({ searchResult })
+                    this.setState({ searchResults })
                 }
             })
+            // if there is no query, then show on result
+        } else {
+            this.setState({ searchResults: [] })
         }
     }
+  
+    render(){
+        //asssign searched books to the "none" shelf unless they are already on the shelf, then display correct shelf
+        this.state.searchResults.map((searchResult) =>{
+            searchResult.shelf ="none"
+            this.props.books.map((book) => {
+                searchResult.id === book.id ? searchResult.shelf=book.shelf : ''}
+            )})
 
+        return(
+
+            <div className="search-books">
+            <div className="search-books-bar">
+              <Link className="close-search" to="/">Close</Link>  
+              <div className="search-books-input-wrapper">
+
+                <input 
+                    type="text" 
+                    placeholder="Search by title or author"
+                    value={this.state.request}
+                    onChange={(event) => this.updateSearch(event.target.value)}
+                    />
+              </div>
+            </div>
+
+            <div className="search-books-results">
+              <ol className="books-grid">
+                {this.state.searchResults
+                    .map((searchResult)=>
+                    <li key={searchResult.id}>
+                        <Book 
+                            bookID ={searchResult.id}
+                            image ={searchResult.imageLinks}
+                            title ={searchResult.title}
+                            authors={searchResult.authors}
+                            updateShelf={this.props.updateShelf}
+                            currentShelf={searchResult.shelf}
+                        />
+                    </li>
+                    )
+                }
+              </ol>
+            </div>
+          </div>
+
+        )
+    }
 }
 
 export default BookSearch;
